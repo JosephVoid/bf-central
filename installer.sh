6e92b77b0388149c5dd348af00d545c02a58f87b
Yoseph <yosephten@gmail.com>
2024-01-20 21:36:03 +0300

    `fix`: auto-installer fix

--- installer.sh
+++ installer.sh
@@ -11,6 +11,7 @@ echo -e "
 
 # Recieve data/secrets from the user
 read -p "Enter MySQL DB URL (Press Enter for jdbc:mysql://mysqldb:3306/buyersfirstdb): " db_url;
+read -p "Enter MySQL DB Host (Press Enter for mysqldb: " db_host;
 read -p "Enter MySQL DB USER (Press Enter for root): " db_user;
 read -p "Enter MySQL DB PASSWORD: " db_pass;
 read -p "Enter JWT Issuer (Press Enter for BUYERSFIRST): " jwt_iss;
@@ -23,6 +24,10 @@ read -p "Enter REDIS Passowrd : " rds_pass;
 
 if [[ -z "$db_url" ]]; then
     db_url="jdbc:mysql://mysqldb:3306/buyersfirstdb";
+fi
+
+if [[ -z "$db_host" ]]; then
+    db_host="mysqldb";
 fi
 
 if [[ -z "$db_user" ]]; then
@@ -54,6 +59,12 @@ db_name=$(echo "${db_url##*/}")
 # Clone all the submodules
 git submodule update -i
 
+# Update the submodules
+cd auth ; git checkout main ; git pull ; cd ..
+cd chat ; git checkout main ; git pull ; cd ..
+cd core ; git checkout main ; git pull ; cd ..
+cd file ; git checkout main ; git pull ; cd ..
+
 # Generate and place the secret files
 ## For the core repo
 echo "DB_URL=$db_url
@@ -76,6 +87,15 @@ JWT_SEC=$jwt_sec
 JWT_EXP=$jwt_exp
 " > auth/src/main/resources/docker.secret.properties
 
+# ## For the chat repo
+echo "PORT=4000
+DB_HOST=$db_host
+DB_USER=$db_user
+DB_PASS=$db_pass
+DB_NAME=$db_name
+JWT_SEC=$jwt_sec
+" > chat/.env
+
 ## For the .env
 echo "DB_NAME=$db_name
 DB_PASS=$db_pass
@@ -85,6 +105,7 @@ REDIS_PASS=$rds_pass
 # Remove application.properties file [bc it smh overrides the application-docker.properties]
 rm auth/src/main/resources/application.properties
 rm core/src/main/resources/application.properties
+rm file/src/main/resources/application.properties
 # Build images and Containers
 docker compose build
 
